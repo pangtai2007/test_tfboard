7ab91a148542cadc6553f6f063c7b48a7972bf09
Hummpy <2764182576@qq.com>
2017-11-13 18:32:26 +0800

    today

--- session.py
+++ session.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
+import numpy as np
 
-input1 = tf.constant([1.0,2.0,3.0], name = "input1")
-input2 = tf.constant([1.0,2.0,3.0], name = "input2")
+with tf.name_scope("input"):
+    input1 = tf.constant([1.0,2.0,3.0])
+    input2 = tf.constant([1.0,2.0,3.0])
 
 output = tf.add_n([input1, input2], name = "add")
+
+#tf.summary.scalar('input1', input1)
+#tf.summary.scalar('input2', input2)
+tf.summary.scalar('output', output)
 
 sess = tf.Session()
 sess.run(output)
 
-writer = tf.summary.FileWriter("/home/dev/Workspace/workk/log", tf.get_default_graph())
+writer = tf.summary.FileWriter("log", tf.get_default_graph())
 writer.close()
